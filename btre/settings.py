d4baeba9a092473ec6ff189ac586e1f1624bc742
Sumanmit Padhi <sumanmit.padhi@outlook.com>
2018-12-17 01:38:51 +0530

    Add email support for enquiries.

--- btre/settings.py
+++ btre/settings.py
@@ -141,3 +141,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger"
 }
+
+#Email Config
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "dx5133057@gmail.com"
+EMAIL_HOST_PASSWORD = "Albvus$dx5"
+EMAIL_USE_TLS = True
